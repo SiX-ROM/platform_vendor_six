73895ee0a829575bdc19b345d4acc0379f8bda4a
R3Ds <fpoolboy@gmail.com>
2017-08-07 13:11:02 -0400

    Shamu: Update fingerprint to N8I11B (Aug 2017)

--- products/shamu.mk
+++ products/shamu.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := motorola
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27H/4072753:user/release-keys \
-    PRIVATE_BUILD_DESC="N2G48B"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
+    PRIVATE_BUILD_DESC="N2G48C"
