1bfca4225971094002b870d723a76fdef65d5fa0
R3Ds <fpoolboy@gmail.com>
2017-10-04 07:53:26 -0400

    Shamu: Update fingerprint to NGI77B (Oct 2017)

--- products/shamu.mk
+++ products/shamu.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := motorola
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
-    PRIVATE_BUILD_DESC="N2G48C"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/NGI77B/4345728:user/release-keys \
+    PRIVATE_BUILD_DESC="NGI77B"
