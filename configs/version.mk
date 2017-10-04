ffcbc2f055c4911aeda18e5c13dbebb16a503e24
R3Ds <fpoolboy@gmail.com>
2017-10-04 08:34:02 -0400

    version: Update to v5.1

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 
 #SiX versioning
 ifndef SIX_BUILD_TYPE
-    SIX_BUILD_TYPE := N2G48B-v4.0-R3Ds
+    SIX_BUILD_TYPE := NGI77B-v5.1-R3Ds
 endif
 
 SIX_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(SIX_BUILD_TYPE)
